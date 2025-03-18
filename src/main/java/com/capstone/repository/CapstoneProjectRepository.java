535f437e1583dd7842f711320c3753a6e9ff6851
meg <aithalmeghana21@gmail.com>
2025-03-18 14:19:58 +0530

    starting with a clean slate

--- src/main/java/com/capstone/repository/CapstoneProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.repository;
-
-import com.capstone.model.CapstoneProject;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CapstoneProjectRepository extends MongoRepository<CapstoneProject, String> {
-}
